9044fc8604fa336f73469a1d02bfc907c2ab3f1b
ajobi <andrej.bilec@gmail.com>
2020-06-14 16:18:01 +0200

    :package: New: Returning empty string if max lines is <= 0

--- src/modules/onElementResized.js
+++ src/modules/onElementResized.js
@@ -1,6 +1,11 @@
 import { ElementSnipper } from './ElementSnipper.js'
 
 export const snipElement = (el) => {
+  if (el.__snipText.maxLines <= 0) {
+    el.innerText = ''
+    return
+  }
+
   new ElementSnipper(el)
     .snipSentences()
     .snipWords()
