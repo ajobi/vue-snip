bc77c645010dccdcd938531b75f2cfdf7cf686cc
ajobi <andrej.bilec@gmail.com>
2020-06-14 13:01:20 +0200

    :package: New: First logic for diretive setup added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+import { snipElement } from './modules/onElementResized.js'
+
 export default {
   install (Vue, options) {
     Vue.directive('snip-text', {
-      bind (el, binding, vnode, oldVnode) {
-        console.log(el)
+      inserted (el, { value }) {
+        el.__snipText = {
+          fullText: el.innerText,
+          maxLines: value
+        }
+
+        const resizeObserver = new ResizeObserver(() => snipElement(el))
+        resizeObserver.observe(el)
+      },
+      update (el, { value }) {
+        el.__snipText.maxLines = value
+        snipElement(el)
       }
     })
   }
