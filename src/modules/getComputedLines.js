91cc7bd32b747e242c5960b741cecb40c6ffa7c2
ajobi <andrej.bilec@gmail.com>
2020-06-14 11:44:19 +0200

    :package: New: Logic for calculating rendered lines added

---
+++ src/modules/getComputedLines.js
@@ -0,0 +1,10 @@
+export const getComputedLines = el => {
+  const computedStyle = window.getComputedStyle(el)
+
+  const computedHeight = parseInt(computedStyle.height)
+  const computedLineHeight = computedStyle.lineHeight === 'normal'
+    ? parseInt(computedStyle.fontSize) * 1.2
+    : parseInt(computedStyle.lineHeight)
+
+  return Math.ceil(computedHeight / computedLineHeight)
+}
