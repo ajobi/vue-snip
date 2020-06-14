ba3695fe85e6c419343f8330e9292d759a70aa4c
ajobi <andrej.bilec@gmail.com>
2020-06-14 19:53:23 +0200

    :package: New: library target specified in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'amd'
   }
 }
