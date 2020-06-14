e07937724cb46f4a20c6d1a55aa1ca9ad1d79212
ajobi <andrej.bilec@gmail.com>
2020-06-14 17:19:34 +0200

    :ok_hand: Refactor: UA lineheight moved into a named constant

--- src/modules/getLines.js
+++ src/modules/getLines.js
@@ -1,9 +1,10 @@
+const UA_LINE_HEIGHT = 1.2
+
 export const getLines = el => {
   const computedStyle = window.getComputedStyle(el)
-
   const computedHeight = parseInt(computedStyle.height)
   const computedLineHeight = computedStyle.lineHeight === 'normal'
-    ? parseInt(computedStyle.fontSize) * 1.2
+    ? parseInt(computedStyle.fontSize) * UA_LINE_HEIGHT
     : parseInt(computedStyle.lineHeight)
 
   return Math.ceil(computedHeight / computedLineHeight)
