69d49b650bd2fa2d8f39f89d2f26e7908a467978
ajobi <andrej.bilec@gmail.com>
2020-06-14 11:45:38 +0200

    :package: New: Resize listener using the snipper class added

---
+++ src/modules/onElementResized.js
@@ -0,0 +1,8 @@
+import { ElementSnipper } from './ElementSnipper.js'
+
+export const snipElement = (el) => {
+  new ElementSnipper(el)
+    .snipSentences()
+    .snipWords()
+    .snipCharacters()
+}
