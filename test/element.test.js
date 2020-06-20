af4e2489881305f05fcdf021f550bff5949f1e57
Andrej Bilec <andrej.bilec@gmail.com>
2020-06-20 17:48:07 +0200

    :white_check_mark: Test: Tests for the line count calculation with implicit line height added

--- test/element.test.js
+++ test/element.test.js
@@ -1,7 +1,28 @@
+import { elementLines } from '../src/element/element.lines.js'
+
+const getImplicitStyles = (height) => ({
+  lineHeight: 'normal',
+  fontSize: '10px',
+  height: height
+})
+
 describe('elementLines', () => {
-  test('test', () => {
-    expect(true).toEqual(true)
+  describe('with implicit lineheight', () => {
+    test('uses 1.2 lineheight', () => {
+      window.getComputedStyle = () => getImplicitStyles('120px')
+      expect(elementLines()).toEqual(10)
+    })
+
+    test('rounds the decimal result up', () => {
+      window.getComputedStyle = () => getImplicitStyles('119px')
+      expect(elementLines()).toEqual(10)
+
+      window.getComputedStyle = () => getImplicitStyles('121px')
+      expect(elementLines()).toEqual(11)
+    })
   })
+
+  describe('with explicit lineheight', () => {})
 })
 
 describe('ElementSnipper', () => {
