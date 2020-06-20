8db211c77b34ddafd4b663630a1917ca87b2ba83
Andrej Bilec <andrej.bilec@gmail.com>
2020-06-20 15:18:03 +0200

    :package: New: Babel loader with IE11 target setup

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'amd'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: { ie: '11' } }]
+          ]
+        }
+      }
+    }]
   }
 }
