3f41f278b234c9ded151c387f5142e574247bb37
Andrej Bilec <andrej.bilec@gmail.com>
2020-06-20 19:54:25 +0200

    :white_check_mark: Test: snipText unit tests for early opt out added

--- test/element.test.js
+++ test/element.test.js
@@ -1,4 +1,6 @@
 import { elementLines } from '../src/element/element.lines.js'
+import { ElementSnipper } from '../src/element/element.snipper'
+import { getSnipText } from '../src/element'
 
 const getMockStyles = (height, lineHeight, fontSize) => ({
   height: height || '100px',
@@ -33,10 +35,46 @@ describe('elementLines', () => {
   })
 })
 
-describe('ElementSnipper', () => {
+const getMockState = (maxLines = 3, ellipsis = '...') => {
+  const element = {
+    innerText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci alias asperiores consectetur delectus dolore dolorem doloremque eveniet ex facilis fuga impedit itaque iure libero magnam necessitatibus, nemo nihil nostrum perspiciatis provident quae ratione rem saepe sunt tempora velit veritatis voluptatibus. Amet atque dolor ea excepturi hic maxime molestiae quam repellendus soluta tempora. Accusamus at ea eligendi, error facere in ipsa labore minima natus.'
+  }
 
-})
+  const elementMap = new WeakMap()
+  elementMap.set(element, {
+    fullText: element.innerText,
+    maxLines: maxLines
+  })
 
-describe('getSnipText', () => {
+  const mockState = {
+    elementMap,
+    options: {
+      ellipsis
+    }
+  }
 
+  return {
+    element,
+    mockState
+  }
+}
+
+describe('snipText', () => {
+  it('opts out on zero maxLines properly', () => {
+    const { element, mockState } = getMockState(0)
+    const snipText = getSnipText(mockState)
+
+    snipText(element)
+
+    expect(element.innerText).toEqual('')
+  })
+
+  it('opts out on negative maxLines properly', () => {
+    const { element, mockState } = getMockState(-1)
+    const snipText = getSnipText(mockState)
+
+    snipText(element)
+
+    expect(element.innerText).toEqual('')
+  })
 })
