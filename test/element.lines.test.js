341e0c8cb5b25cbcb6f8bfab4c3363b414acc1c2
Andrej Bilec <andrej.bilec@gmail.com>
2020-06-21 07:52:08 +0200

    :white_check_mark: Test: Element tests split into separate files

---
+++ test/element.lines.test.js
@@ -0,0 +1,39 @@
+import { elementLines } from '../src/element/element.lines.js'
+
+const getMockStyles = (height, lineHeight, fontSize) => ({
+  height: height || '100px',
+  lineHeight: lineHeight || 'normal',
+  fontSize: fontSize || '10px'
+})
+
+let spy
+beforeAll(() => {
+  spy = jest.spyOn(window, 'getComputedStyle')
+})
+
+describe('elementLines', () => {
+  describe('with implicit lineheight', () => {
+    it('uses 1.2 effective lineheight', () => {
+      spy.mockReturnValue(getMockStyles('120px'))
+      expect(elementLines()).toEqual(10)
+    })
+
+    it('rounds the decimal result up', () => {
+      spy.mockReturnValue(getMockStyles('119px'))
+      expect(elementLines()).toEqual(10)
+
+      spy.mockReturnValue(getMockStyles('121px'))
+      expect(elementLines()).toEqual(11)
+    })
+  })
+
+  describe('with explicit lineheight', () => {
+    it('respects the explicit line-height', () => {
+      spy.mockReturnValue(getMockStyles('120px', '20px'))
+      expect(elementLines()).toEqual(6)
+
+      spy.mockReturnValue(getMockStyles('121px', '20px'))
+      expect(elementLines()).toEqual(7)
+    })
+  })
+})
--- test/element.test.js
+++ test/element.test.js
@@ -1,4 +1,3 @@
-import { elementLines } from '../src/element/element.lines.js'
 import { getSnipText } from '../src/element'
 
 const getMockStyles = (height, lineHeight, fontSize) => ({
@@ -10,33 +9,6 @@ const getMockStyles = (height, lineHeight, fontSize) => ({
 let spy
 beforeAll(() => {
   spy = jest.spyOn(window, 'getComputedStyle')
-})
-
-describe('elementLines', () => {
-  describe('with implicit lineheight', () => {
-    it('uses 1.2 effective lineheight', () => {
-      spy.mockReturnValue(getMockStyles('120px'))
-      expect(elementLines()).toEqual(10)
-    })
-
-    it('rounds the decimal result up', () => {
-      spy.mockReturnValue(getMockStyles('119px'))
-      expect(elementLines()).toEqual(10)
-
-      spy.mockReturnValue(getMockStyles('121px'))
-      expect(elementLines()).toEqual(11)
-    })
-  })
-
-  describe('with explicit lineheight', () => {
-    it('respects the explicit line-height', () => {
-      spy.mockReturnValue(getMockStyles('120px', '20px'))
-      expect(elementLines()).toEqual(6)
-
-      spy.mockReturnValue(getMockStyles('121px', '20px'))
-      expect(elementLines()).toEqual(7)
-    })
-  })
 })
 
 const getMockState = (maxLines = 3, ellipsis = '...') => {
