4c325f7f4300bd4d8c7634395236fb1508d9fd31
Andrej Bilec <andrej.bilec@gmail.com>
2020-06-21 08:53:50 +0200

    :white_check_mark: Test: innerText in snipper replaced by textContent (additional test adjustments)

--- test/element.test.js
+++ test/element.test.js
@@ -6,19 +6,21 @@ const getMockStyles = (height, lineHeight, fontSize) => ({
   fontSize: fontSize || '10px'
 })
 
+let element
 let spy
 beforeAll(() => {
+  document.body.innerHTML = `
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci alias asperiores consectetur delectus dolore dolorem doloremque eveniet ex facilis fuga impedit itaque iure libero magnam necessitatibus, nemo nihil nostrum perspiciatis provident quae ratione rem saepe sunt tempora velit veritatis voluptatibus. Amet atque dolor ea excepturi hic maxime molestiae quam repellendus soluta tempora. Accusamus at ea eligendi, error facere in ipsa labore minima natus.</p>
+    `
+
   spy = jest.spyOn(window, 'getComputedStyle')
+  element = document.querySelector('p')
 })
 
-const getMockState = (maxLines = 3, ellipsis = '...') => {
-  const element = {
-    textContent: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci alias asperiores consectetur delectus dolore dolorem doloremque eveniet ex facilis fuga impedit itaque iure libero magnam necessitatibus, nemo nihil nostrum perspiciatis provident quae ratione rem saepe sunt tempora velit veritatis voluptatibus. Amet atque dolor ea excepturi hic maxime molestiae quam repellendus soluta tempora. Accusamus at ea eligendi, error facere in ipsa labore minima natus.'
-  }
-
+const getMockState = (element, maxLines = 3, ellipsis = '...') => {
   const elementMap = new WeakMap()
   elementMap.set(element, {
-    fullText: element.innerText,
+    fullText: element.textContent,
     maxLines: maxLines
   })
 
@@ -30,14 +32,13 @@ const getMockState = (maxLines = 3, ellipsis = '...') => {
   }
 
   return {
-    element,
     mockState
   }
 }
 
 describe('snipText', () => {
   it('opts out on zero maxLines properly', () => {
-    const { element, mockState } = getMockState(0)
+    const { mockState } = getMockState(element, 0)
     const snipText = getSnipText(mockState)
 
     snipText(element)
@@ -46,7 +47,7 @@ describe('snipText', () => {
   })
 
   it('opts out on negative maxLines properly', () => {
-    const { element, mockState } = getMockState(-1)
+    const { mockState } = getMockState(element, -1)
     const snipText = getSnipText(mockState)
 
     snipText(element)
@@ -56,9 +57,9 @@ describe('snipText', () => {
 
   it('opts out on within line range properly', () => {
     spy.mockReturnValue(getMockStyles())
-    const { element, mockState } = getMockState(20)
+    const { mockState } = getMockState(element, 20)
     const snipText = getSnipText(mockState)
-    const originalText = element.innerText
+    const originalText = element.textContent
 
     snipText(element)
 
