7ce34dcd2f2296d4a825b4b129f8661cabb08b3c
Andrej Bilec <andrej.bilec@gmail.com>
2020-06-25 17:36:01 +0200

    :package: New: Separate dev server configuration for test run added
    Took 35 minutes

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
 const path = require('path')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: 'VueSnipText',
-    libraryTarget: 'umd'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      }
-    ]
-  },
-  devServer: {
-    contentBase: 'playground',
-    port: 9000,
-    open: true
+module.exports = (env, argv) => {
+  const { testDevServer } = argv
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      library: 'VueSnipText',
+      libraryTarget: 'umd'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        }
+      ]
+    },
+    devServer: {
+      contentBase: 'playground',
+      port: testDevServer ? 9001 : 9000,
+      open: !testDevServer
+    }
   }
 }
