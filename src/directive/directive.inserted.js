3c00610a70949a553e254a6a434e17fce9813e20
Andrej Bilec <andrej.bilec@gmail.com>
2020-07-03 20:12:49 +0200

    :wrench: Fix: Sniping only allowed on the width change (not any resize observation)
    Took 29 minutes

--- src/directive/directive.inserted.js
+++ src/directive/directive.inserted.js
@@ -1,7 +1,12 @@
 export const getInserted = (state, snipText) => (el, { value, arg }) => {
   const { elementMap, options } = state
 
-  const observer = new ResizeObserver(() => snipText(el))
+  const observer = new ResizeObserver(([entry]) => {
+    if (entry.contentRect.width !== elementMap.get(el).prevWidth) {
+      snipText(el)
+      elementMap.get(el).prevWidth = el.clientWidth
+    }
+  })
   observer.observe(el)
 
   elementMap.set(el, {
