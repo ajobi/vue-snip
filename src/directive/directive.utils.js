ac92dd3d4d11c9f7ded8553ccaab95d1d19df0a4
Andrej Bilec <andrej.bilec@gmail.com>
2020-07-11 19:24:26 +0200

    :package: New: Normalize snip method utility function added
    Took 12 minutes

--- src/directive/directive.utils.js
+++ src/directive/directive.utils.js
@@ -1,4 +1,20 @@
+import { defaultOptions } from '../defaultOptions'
+
 export const normalizeMaxLines = (maxLines) => {
   const parsedMaxLines = parseInt(maxLines)
   return Number.isNaN(parsedMaxLines) ? 0 : parsedMaxLines
+}
+
+export const normalizeSnipMethod = (state, snipMethod) => {
+  if (snipMethod === 'css' || snipMethod === 'js') {
+    return snipMethod
+  }
+
+  const { snipMethod: globalSnipMethod } = state.options
+
+  if (globalSnipMethod === 'css' || globalSnipMethod === 'js') {
+    return globalSnipMethod
+  }
+
+  return defaultOptions.snipMethod
 }
