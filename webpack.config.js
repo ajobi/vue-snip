6a8b9d940112fd17794b571161c773eca5f640e4
Andrej Bilec <andrej.bilec@gmail.com>
2020-07-11 20:00:38 +0200

    :ok_hand: Improve: Webpack output library name changed to VueSnip
    Took 3 minutes

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = () => {
     output: {
       filename: 'main.js',
       path: path.resolve(__dirname, 'dist'),
-      library: 'VueSnipText',
+      library: 'VueSnip',
       libraryTarget: 'umd'
     },
     module: {
