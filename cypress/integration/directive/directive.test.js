f4f6f95c916953853e6b6fa468c68a9cdc19cc97
Andrej Bilec <andrej.bilec@smilingwords.eu>
2020-07-13 21:52:16 +0200

    :white_check_mark: Test: First tests for directive added

--- cypress/integration/directive/directive.test.js
+++ cypress/integration/directive/directive.test.js
@@ -1,9 +1,69 @@
+import { defaultOptions } from '../../../src/defaultOptions'
+
+const { maxLines, snipMethod } = defaultOptions
+
 describe('Directive', () => {
   beforeEach(() => {
     cy.visit('/directive')
   })
 
-  it('', () => {
-    expect(true).to.equal(true)
+  describe('Paragraph without value and parameters', () => {
+    it('Does always use default maxLines', () => {
+      cy.window().then(window => {
+        cy.get('[data-cy=paragraphOne]').then(([parOne]) => {
+          const elementState = window.__VueSnipState.elementMap.get(parOne)
+
+          expect(elementState.maxLines).to.equal(maxLines)
+
+          cy.get('[data-cy=linesPlus]').click().then(() => {
+            expect(elementState.maxLines).to.equal(maxLines)
+          })
+        })
+      })
+    })
+
+    it('Does always use default snipping method', () => {
+      cy.window().then(window => {
+        cy.get('[data-cy=paragraphOne]').then(([parOne]) => {
+          const elementState = window.__VueSnipState.elementMap.get(parOne)
+
+          expect(elementState.snipMethod).to.equal(snipMethod)
+
+          cy.get('[data-cy=methodToggle]').click().then(() => {
+            expect(elementState.snipMethod).to.equal(snipMethod)
+          })
+        })
+      })
+    })
+  })
+
+  describe('Paragraph with value and without parameters', () => {
+    it('Reflects the directive value changes', () => {
+      cy.window().then(window => {
+        cy.get('[data-cy=paragraphTwo]').then(([parTwo]) => {
+          const elementState = window.__VueSnipState.elementMap.get(parTwo)
+
+          expect(elementState.maxLines).to.equal(maxLines)
+
+          cy.get('[data-cy=linesPlus]').click().then(() => {
+            expect(elementState.maxLines).to.equal(4)
+          })
+        })
+      })
+    })
+
+    it('Does always use default snipping method', () => {
+      cy.window().then(window => {
+        cy.get('[data-cy=paragraphTwo]').then(([parTwo]) => {
+          const elementState = window.__VueSnipState.elementMap.get(parTwo)
+
+          expect(elementState.snipMethod).to.equal(snipMethod)
+
+          cy.get('[data-cy=methodToggle]').click().then(() => {
+            expect(elementState.snipMethod).to.equal(snipMethod)
+          })
+        })
+      })
+    })
   })
 })
