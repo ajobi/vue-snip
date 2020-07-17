22b91511ce096695989d3987c20932f25816a892
Andrej Bilec <andrej.bilec@smilingwords.eu>
2020-07-17 21:08:21 +0200

    :white_check_mark: Test: Directive unbind tests added

---
+++ cypress/integration/directive/directive.unbind.test.js
@@ -0,0 +1,43 @@
+describe('Directive Unbind', () => {
+  beforeEach(() => {
+    cy.visit('/directive')
+  })
+
+  it('Unbinds properly', () => {
+    cy.window().then(window => {
+      const { elementMap } = window.__VueSnipState
+
+      let paragraph1 = null
+      let paragraph2 = null
+      let paragraph3 = null
+      let paragraph4 = null
+
+      cy.get('[data-cy=paragraph1]').then(([paragraph]) => {
+        expect(elementMap.has(paragraph)).to.equal(true)
+        paragraph1 = paragraph
+      })
+
+      cy.get('[data-cy=paragraph2]').then(([paragraph]) => {
+        paragraph2 = paragraph
+        expect(elementMap.has(paragraph)).to.equal(true)
+      })
+
+      cy.get('[data-cy=paragraph3]').then(([paragraph]) => {
+        paragraph3 = paragraph
+        expect(elementMap.has(paragraph)).to.equal(true)
+      })
+
+      cy.get('[data-cy=paragraph4]').then(([paragraph]) => {
+        paragraph4 = paragraph
+        expect(elementMap.has(paragraph)).to.equal(true)
+      })
+
+      cy.get('[data-cy=visibilityToggle]').click().then(() => {
+        expect(elementMap.has(paragraph1)).to.equal(false)
+        expect(elementMap.has(paragraph2)).to.equal(false)
+        expect(elementMap.has(paragraph3)).to.equal(false)
+        expect(elementMap.has(paragraph4)).to.equal(false)
+      })
+    })
+  })
+})
