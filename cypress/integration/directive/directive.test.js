527022c33a745ab50534775324b30be98ef0d69d
Andrej Bilec <andrej.bilec@smilingwords.eu>
2020-07-17 20:45:41 +0200

    :white_check_mark: Test: First directive tests added

--- cypress/integration/directive/directive.test.js
+++ cypress/integration/directive/directive.test.js
@@ -1,69 +1,75 @@
-// import { defaultOptions } from '../../../src/defaultOptions'
-//
-// const { maxLines, snipMethod } = defaultOptions
-//
-// describe('Directive', () => {
-//   beforeEach(() => {
-//     cy.visit('/directive')
-//   })
-//
-//   describe('Paragraph without value and parameters', () => {
-//     it('Does always use default maxLines', () => {
-//       cy.window().then(window => {
-//         cy.get('[data-cy=paragraphOne]').then(([parOne]) => {
-//           const elementState = window.__VueSnipState.elementMap.get(parOne)
-//
-//           expect(elementState.maxLines).to.equal(maxLines)
-//
-//           cy.get('[data-cy=linesPlus]').click().then(() => {
-//             expect(elementState.maxLines).to.equal(maxLines)
-//           })
-//         })
-//       })
-//     })
-//
-//     it('Does always use default snipping method', () => {
-//       cy.window().then(window => {
-//         cy.get('[data-cy=paragraphOne]').then(([parOne]) => {
-//           const elementState = window.__VueSnipState.elementMap.get(parOne)
-//
-//           expect(elementState.snipMethod).to.equal(snipMethod)
-//
-//           cy.get('[data-cy=methodToggle]').click().then(() => {
-//             expect(elementState.snipMethod).to.equal(snipMethod)
-//           })
-//         })
-//       })
-//     })
-//   })
-//
-//   describe('Paragraph with value and without parameters', () => {
-//     it('Reflects the directive value changes', () => {
-//       cy.window().then(window => {
-//         cy.get('[data-cy=paragraphTwo]').then(([parTwo]) => {
-//           const elementState = window.__VueSnipState.elementMap.get(parTwo)
-//
-//           expect(elementState.maxLines).to.equal(maxLines)
-//
-//           cy.get('[data-cy=linesPlus]').click().then(() => {
-//             expect(elementState.maxLines).to.equal(4)
-//           })
-//         })
-//       })
-//     })
-//
-//     it('Does always use default snipping method', () => {
-//       cy.window().then(window => {
-//         cy.get('[data-cy=paragraphTwo]').then(([parTwo]) => {
-//           const elementState = window.__VueSnipState.elementMap.get(parTwo)
-//
-//           expect(elementState.snipMethod).to.equal(snipMethod)
-//
-//           cy.get('[data-cy=methodToggle]').click().then(() => {
-//             expect(elementState.snipMethod).to.equal(snipMethod)
-//           })
-//         })
-//       })
-//     })
-//   })
-// })
+import { defaultOptions } from '../../../src/defaultOptions'
+
+const { maxLines, snipMethod } = defaultOptions
+
+describe('Directive', () => {
+  beforeEach(() => {
+    cy.visit('/directive')
+  })
+
+  describe('Paragraph without value and parameters', () => {
+    it('Does always use default maxLines', () => {
+      cy.window().then(window => {
+        cy.get('[data-cy=paragraph1]').then(([paragraph]) => {
+          const elementState = window.__VueSnipState.elementMap.get(paragraph)
+
+          expect(elementState.maxLines).to.equal(maxLines)
+
+          cy.get('[data-cy=linesPlus]').click().then(() => {
+            expect(elementState.maxLines).to.equal(maxLines)
+          })
+        })
+      })
+    })
+
+    it('Does always use default snipping method', () => {
+      cy.window().then(window => {
+        cy.get('[data-cy=paragraph1]').then(([paragraph]) => {
+          const elementState = window.__VueSnipState.elementMap.get(paragraph)
+
+          expect(elementState.snipMethod).to.equal(snipMethod)
+
+          cy.get('[data-cy=methodToggle]').click().then(() => {
+            expect(elementState.snipMethod).to.equal(snipMethod)
+          })
+        })
+      })
+    })
+  })
+
+  describe('Paragraph with value', () => {
+    it('Reflects the value changes', () => {
+      cy.window().then(window => {
+        cy.get('[data-cy=paragraph2]').then(([paragraph]) => {
+          const elementState = window.__VueSnipState.elementMap.get(paragraph)
+
+          expect(elementState.maxLines).to.equal(maxLines)
+
+          cy.get('[data-cy=linesPlus]').click().then(() => {
+            expect(elementState.maxLines).to.equal(4)
+          })
+        })
+      })
+    })
+  })
+
+  describe('Paragraph with argument', () => {
+    it('Reflects the argument changes', () => {
+      cy.window().then(window => {
+        cy.get('[data-cy=paragraph3]').then(([paragraph]) => {
+          const elementState = window.__VueSnipState.elementMap.get(paragraph)
+
+          expect(elementState.snipMethod).to.equal(snipMethod)
+
+          cy.get('[data-cy=methodToggle]').click().then(() => {
+            expect(elementState.snipMethod).to.equal('js')
+          })
+
+          cy.get('[data-cy=methodToggle]').click().then(() => {
+            expect(elementState.snipMethod).to.equal('css')
+          })
+        })
+      })
+    })
+  })
+})
