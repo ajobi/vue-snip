40ad30983e747529f65b9429a772f122c74f25ac
Andrej Bilec <andrej.bilec@smilingwords.eu>
2020-07-18 16:40:28 +0200

    :ok_hand: Improve: General vue-snip tests added and merged with install test

--- cypress/integration/vue-snip.test.js
+++ cypress/integration/vue-snip.test.js
@@ -1,14 +1,27 @@
-import { defaultOptions } from '../../../src/defaultOptions'
+import { defaultOptions } from '../../src/defaultOptions'
+import plugin from '../../src/index.js'
 
 const { maxLines, snipMethod } = defaultOptions
 
-describe('Directive', () => {
+describe('Vue Snip', () => {
   beforeEach(() => {
     cy.visit('/directive')
   })
 
-  describe('Without explicit value or parameters', () => {
-    it('Does always use default maxLines', () => {
+  describe('Installation', () => {
+    it('Does not expose the state without debugMode', () => {
+      plugin.install({ directive: () => {} }, { debugMode: false })
+      expect(window.__VueSnipState).equal(undefined)
+    })
+
+    it('Exposes the state on debugMode', () => {
+      plugin.install({ directive: () => {} }, { debugMode: true })
+      expect(window.__VueSnipState).not.equal(undefined)
+    })
+  })
+
+  describe('Maxlines used', () => {
+    it('Uses default max lines without explicit value', () => {
       cy.window().then(window => {
         cy.get('[data-cy=paragraph1]').then(([paragraph]) => {
           const elementState = window.__VueSnipState.elementMap.get(paragraph)
@@ -22,23 +35,7 @@ describe('Directive', () => {
       })
     })
 
-    it('Does always use default snipping method', () => {
-      cy.window().then(window => {
-        cy.get('[data-cy=paragraph1]').then(([paragraph]) => {
-          const elementState = window.__VueSnipState.elementMap.get(paragraph)
-
-          expect(elementState.snipMethod).to.equal(snipMethod)
-
-          cy.get('[data-cy=methodToggle]').click().then(() => {
-            expect(elementState.snipMethod).to.equal(snipMethod)
-          })
-        })
-      })
-    })
-  })
-
-  describe('With explicit value', () => {
-    it('Reflects the value changes', () => {
+    it('Uses explicit maxlines if given', () => {
       cy.window().then(window => {
         cy.get('[data-cy=paragraph2]').then(([paragraph]) => {
           const elementState = window.__VueSnipState.elementMap.get(paragraph)
@@ -53,8 +50,22 @@ describe('Directive', () => {
     })
   })
 
-  describe('With explicit argument', () => {
-    it('Reflects the argument changes', () => {
+  describe('Method used', () => {
+    it('Uses default method without explicit argument', () => {
+      cy.window().then(window => {
+        cy.get('[data-cy=paragraph1]').then(([paragraph]) => {
+          const elementState = window.__VueSnipState.elementMap.get(paragraph)
+
+          expect(elementState.snipMethod).to.equal(snipMethod)
+
+          cy.get('[data-cy=methodToggle]').click().then(() => {
+            expect(elementState.snipMethod).to.equal(snipMethod)
+          })
+        })
+      })
+    })
+
+    it('Uses explicit method if given', () => {
       cy.window().then(window => {
         cy.get('[data-cy=paragraph3]').then(([paragraph]) => {
           const elementState = window.__VueSnipState.elementMap.get(paragraph)
