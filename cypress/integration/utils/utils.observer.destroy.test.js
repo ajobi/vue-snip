32300abdd6fcf89e00813c6942859d4a3e036074
Andrej Bilec <andrej.bilec@smilingwords.eu>
2020-07-18 18:07:58 +0200

    :white_check_mark: Test: Observer destroy basic test added

---
+++ cypress/integration/utils/utils.observer.destroy.test.js
@@ -0,0 +1,23 @@
+import { addObserver, destroyObserver } from '../../../src/utils'
+
+describe('addObserver', () => {
+  beforeEach(() => {
+    cy.visit('utils/observer')
+  })
+
+  it('Removes the observer from the element state', () => {
+    cy.get('[data-cy=paragraph]').then(([el]) => {
+      const elementMap = new WeakMap()
+      elementMap.set(el, {})
+
+      const snipText = () => {}
+      const state = { elementMap }
+
+      addObserver(state, snipText, el)
+      destroyObserver(state, el)
+
+      expect(state.elementMap.get(el).observer).eq(undefined)
+      expect(state.elementMap.get(el).observer).eq(undefined)
+    })
+  })
+})
