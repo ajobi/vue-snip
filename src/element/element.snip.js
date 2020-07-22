810aeb46382a1ce4f31e2ce84f6e3d3113eaf97f
Andrej Bilec <andrej.bilec@smilingwords.eu>
2020-07-22 19:02:59 +0200

    :wrench: Fix: Snipping on vertical resize put back (log removed)

--- src/element/element.snip.js
+++ src/element/element.snip.js
@@ -3,8 +3,6 @@ import { snipByCSS, snipByJS } from '../method'
 export const getSnipText = (state) => (el) => {
   const elState = state.elementMap.get(el)
 
-  console.log('Snip')
-
   if (elState.snipMethod === 'css') {
     snipByCSS(state, el)
     return
