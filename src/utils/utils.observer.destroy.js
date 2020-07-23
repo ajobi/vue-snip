06f642d6e9d8e0e0f2f61ac14113efc34bddd265
Andrej Bilec <andrej.bilec@smilingwords.eu>
2020-07-23 23:14:54 +0200

    :wrench: Fix: Missing removal of prev-height added

--- src/utils/utils.observer.destroy.js
+++ src/utils/utils.observer.destroy.js
@@ -3,5 +3,6 @@ export const destroyObserver = (state, el) => {
 
   elState.observer && elState.observer.disconnect()
   elState.observer && delete elState.prevWidth
+  elState.observer && delete elState.prevHeight
   elState.observer && delete elState.observer
 }
