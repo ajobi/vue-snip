853c1756cbe0011ad98c9320f49f17c69b178023
Andrej Bilec <andrej.bilec@gmail.com>
2021-11-25 18:52:53 +0100

    :ok_hand: Improve: Redundant dev server removed

--- rollup.config.js
+++ rollup.config.js
@@ -1,50 +1,44 @@
 import esbuild from 'rollup-plugin-esbuild'
 import { babel } from '@rollup/plugin-babel'
 import pkg from './package.json'
+import serve from 'rollup-plugin-serve'
 
-export const input = 'src/index.js'
-export const buildPlugins = [
-  babel({
-    babelHelpers: 'bundled',
-    presets: [['@babel/preset-env', { targets: { ie: '11' } }]]
-  }),
-  esbuild({ minify: true })
-]
+const input = 'src/index.js'
 
-export default [
-  {
-    input,
-    output: [
-      {
-        file: pkg.module,
-        format: 'esm'
-      },
-      {
-        file: pkg.module.replace('dist', 'docs'),
-        format: 'esm'
-      }
-    ],
-    plugins: buildPlugins
-  },
-  {
-    input,
-    output: [
-      {
-        file: pkg.main,
-        format: 'umd',
-        name: 'VueSnip'
-      },
-      {
-        file: pkg.main.replace('dist', 'docs'),
-        format: 'umd',
-        name: 'VueSnip'
-      },
-      {
-        file: pkg.main.replace('dist', 'cypress/tests'),
-        format: 'umd',
-        name: 'VueSnip'
-      }
-    ],
-    plugins: buildPlugins
+export default () => {
+  const plugins = [
+    babel({
+      babelHelpers: 'bundled',
+      presets: [['@babel/preset-env', { targets: { ie: '11' } }]]
+    }),
+    esbuild({ minify: true })
+  ]
+
+  if (process.env.NODE_ENV === 'development') {
+    plugins.push(serve({ port: 3000, contentBase: 'docs' }))
   }
-]
+
+  return [
+    {
+      input,
+      output: [
+        {
+          file: pkg.main,
+          format: 'umd',
+          name: 'VueSnip'
+        },
+        {
+          file: pkg.main.replace('dist', 'docs'),
+          format: 'umd',
+          name: 'VueSnip'
+        },
+        {
+          file: pkg.main.replace('dist', 'cypress/tests'),
+          format: 'umd',
+          name: 'VueSnip'
+        }
+      ],
+      plugins: plugins
+    }
+  ]
+}
