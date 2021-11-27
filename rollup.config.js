b86cdd29972bdc114aa943be1742e63a5922ee6f
Andrej Bilec <andrej.bilec@gmail.com>
2021-11-27 16:30:11 +0100

    :package: New: Basic babel rollup setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,38 @@
 import esbuild from 'rollup-plugin-esbuild'
+import { babel } from '@rollup/plugin-babel'
 import serve from 'rollup-plugin-serve'
 import pkg from './package.json'
 
 const input = 'src/index.js'
+const plugins = [
+  esbuild({ minify: true }),
+  babel({ babelHelpers: 'bundled' })
+]
 
-export default () => {
-  const plugins = [
-    esbuild({ minify: true })
-  ]
-
-  if (process.env.NODE_ENV === 'development') {
-    plugins.push(serve({ port: 3000, contentBase: 'docs', open: true }))
-  }
-
-  return [
-    {
-      input,
-      output: [
-        {
-          file: pkg.main,
-          format: 'umd',
-          name: 'VueSnip'
-        },
-        {
-          file: pkg.main.replace('dist', 'docs'),
-          format: 'umd',
-          name: 'VueSnip'
-        },
-        {
-          file: pkg.main.replace('dist', 'cypress/tests'),
-          format: 'umd',
-          name: 'VueSnip'
-        }
-      ],
-      plugins: plugins
-    }
-  ]
+if (process.env.NODE_ENV === 'development') {
+  plugins.push(serve({ port: 3000, contentBase: 'docs', open: true }))
 }
+
+export default () => [
+  {
+    input,
+    output: [
+      {
+        file: pkg.main,
+        format: 'umd',
+        name: 'VueSnip'
+      },
+      {
+        file: pkg.main.replace('dist', 'docs'),
+        format: 'umd',
+        name: 'VueSnip'
+      },
+      {
+        file: pkg.main.replace('dist', 'cypress/tests'),
+        format: 'umd',
+        name: 'VueSnip'
+      }
+    ],
+    plugins: plugins
+  }
+]
