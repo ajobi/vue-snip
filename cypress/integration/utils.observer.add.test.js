1bfdf8fd38d0ee973c51befa94bdcc697ce64fc2
Andrej Bilec <andrej.bilec@gmail.com>
2021-11-27 14:27:11 +0100

    :white_check_mark: Test: Observer add (missing cases)

--- cypress/integration/utils.observer.add.test.js
+++ cypress/integration/utils.observer.add.test.js
@@ -1,4 +1,6 @@
 import { addObserver, destroyObserver } from '../../instrumented/utils'
+import { defaultOptions } from '../../instrumented/defaultOptions'
+import { getSnipText } from '../../instrumented/element/element.snip'
 
 describe('addObserver', () => {
   beforeEach(() => {
@@ -8,17 +10,62 @@ describe('addObserver', () => {
   it('Adds the observer to the element state', () => {
     cy.get('[data-cy=paragraph]').then(([el]) => {
       const elementMap = new WeakMap()
+      const state = { elementMap, options: defaultOptions }
+
       elementMap.set(el, {})
 
-      const snipText = () => {}
-      const state = { elementMap }
-
+      const snipText = getSnipText(state)
       expect(state.elementMap.get(el).observer).eq(undefined)
 
       addObserver(state, snipText, el)
 
       expect(state.elementMap.get(el).observer).not.eq(undefined)
       destroyObserver(state, el)
+    })
+  })
+
+  it('Snips the element on resize', () => {
+    cy.get('[data-cy=paragraph]').then(([el]) => {
+      const elementMap = new WeakMap()
+      const state = { elementMap, options: defaultOptions }
+      elementMap.set(el, {})
+
+      const snipText = cy.stub()
+      addObserver(state, snipText, el)
+
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(20).then(() => {
+        expect(snipText).to.have.callCount(1)
+
+        el.style.width = '50%'
+
+        // eslint-disable-next-line cypress/no-unnecessary-waiting
+        cy.wait(20).then(() => {
+          expect(snipText).to.have.callCount(2)
+
+          destroyObserver(state, el)
+        })
+      })
+    })
+  })
+
+  it('Does not snip if the element dimensions did not change', () => {
+    cy.get('[data-cy=paragraph]').then(([el]) => {
+      const elementMap = new WeakMap()
+      const state = { elementMap, options: defaultOptions }
+      elementMap.set(el, {})
+
+      const snipText = cy.stub()
+      const elState = elementMap.get(el)
+      elState.prevWidth = el.clientWidth
+      elState.prevHeight = el.clientHeight
+      addObserver(state, snipText, el)
+
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(20).then(() => {
+        expect(snipText).to.have.callCount(0)
+        destroyObserver(state, el)
+      })
     })
   })
 })
