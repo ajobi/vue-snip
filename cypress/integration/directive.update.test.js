46b921c4f4303753f228403013b49a8c901fa85a
Andrej Bilec <andrej.bilec@gmail.com>
2021-11-27 12:27:54 +0100

    :white_check_mark: Test: Directive update proper unit tests (element snip)

--- cypress/integration/directive.update.test.js
+++ cypress/integration/directive.update.test.js
@@ -1,6 +1,7 @@
 import { getSnipText } from '../../instrumented/element/element.snip'
 import { getUpdate } from '../../instrumented/directive'
 import { defaultOptions } from '../../instrumented/defaultOptions'
+import { elementLines } from '../../instrumented/element/element.lines'
 
 describe('Directive Update', () => {
   beforeEach(() => {
@@ -16,25 +17,32 @@ describe('Directive Update', () => {
       const update = getUpdate(state, snipText)
       elementMap.set(paragraph, {})
 
-      update(paragraph, { value: 3, arg: 'css' })
+      update(paragraph, { value: 3, arg: 'js' })
 
       expect(elementMap.get(paragraph).maxLines).to.equal(3)
-      expect(elementMap.get(paragraph).snipMethod).to.equal('css')
+      expect(elementMap.get(paragraph).snipMethod).to.equal('js')
 
-      update(paragraph, { value: 2, arg: 'js' })
+      update(paragraph, { value: 2, arg: 'css' })
 
       expect(elementMap.get(paragraph).maxLines).to.equal(2)
-      expect(elementMap.get(paragraph).snipMethod).to.equal('js')
+      expect(elementMap.get(paragraph).snipMethod).to.equal('css')
     })
   })
 
-  // it('Snips the element', () => {
-  //   cy.get('[data-cy=paragraph4]').then(([paragraph]) => {
-  //     expect(elementLines(paragraph)).equal(3)
-  //
-  //     cy.get('[data-cy=linesPlus]').click().then(() => {
-  //       expect(elementLines(paragraph)).equal(4)
-  //     })
-  //   })
-  // })
+  it('Snips the element', () => {
+    cy.get('[data-cy=paragraph]').then(([paragraph]) => {
+      const elementMap = new WeakMap()
+      const state = { elementMap, options: defaultOptions }
+
+      const snipText = getSnipText(state)
+      const update = getUpdate(state, snipText)
+      elementMap.set(paragraph, { fullText: paragraph.textContent })
+
+      update(paragraph, { value: 3, arg: 'js' })
+      expect(elementLines(paragraph)).equal(3)
+
+      update(paragraph, { value: 4, arg: 'css' })
+      expect(elementLines(paragraph)).equal(4)
+    })
+  })
 })
