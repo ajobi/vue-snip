da50067a4d4287bea4cdc06839373453be8ab302
Andrej Bilec <andrej.bilec@gmail.com>
2021-11-27 18:12:35 +0100

    :ok_hand: Improve: Removing babel as a way to collect coverage

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,11 +17,5 @@
  */
 module.exports = (on, config) => {
   require('@cypress/code-coverage/task')(on, config)
-  // tell Cypress to use .babelrc file
-  // and instrument the specs files
-  // only the extra application files will be instrumented
-  // not the spec files themselves
-  on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
-
   return config
 }
