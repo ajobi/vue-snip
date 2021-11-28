b4afc0159b3b74794741d5e2a9cd895c765bcebb
Andrej Bilec <andrej.bilec@gmail.com>
2021-11-28 11:55:07 +0100

    :package: New: Typescript eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,14 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
   extends: [
+    'plugin:@typescript-eslint/recommended',
     'standard',
     'plugin:cypress/recommended'
   ]
