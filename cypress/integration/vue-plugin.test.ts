ed90ccef949e5c2c5e544dfe1b0ba7ecaa7625d8
Andrej Bilec <andrej.bilec@gmail.com>
2021-11-30 20:01:56 +0100

    :ok_hand: Improve: TS disabled where interfering with tests

--- cypress/integration/vue-plugin.test.ts
+++ cypress/integration/vue-plugin.test.ts
@@ -3,14 +3,16 @@ import plugin from '../../instrumented/index.js'
 
 const { maxLines, snipMethod } = defaultOptions
 
-const getVue = (version, directive) => {
+const getMockVue = (version?, directive?) => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   const Vue = function () {}
-  Vue.directive = directive || (() => {})
+  Vue.directive = directive || (() => ({}))
   Vue.version = version || '2.0.0'
 
   return Vue
 }
 
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 describe('Vue Plugin', () => {
   beforeEach(() => {
     cy.visit('./cypress/tests/integration-v2.html')
@@ -19,37 +21,42 @@ describe('Vue Plugin', () => {
   describe('Installation', () => {
     describe('Debug mode', () => {
       it('Does not expose the state without debugMode', () => {
-        plugin.install(getVue(), { debugMode: false })
+        // @ts-ignore
+        plugin.install(getMockVue(), { debugMode: false })
         expect(window.__VueSnipState).equal(undefined)
       })
 
       it('Exposes the state on debugMode', () => {
-        plugin.install(getVue(), { debugMode: true })
+        // @ts-ignore
+        plugin.install(getMockVue(), { debugMode: true })
         expect(window.__VueSnipState).not.equal(undefined)
       })
     })
 
     describe('Snip function', () => {
       it('Does not expose the snip function without exposeSnipFunction', () => {
-        const Vue = getVue()
+        const Vue = getMockVue()
 
+        // @ts-ignore
         plugin.install(Vue, { exposeSnipFunction: false })
 
         expect((new Vue()).$snipText).eq(undefined)
       })
 
       it('Exposes the snip function with exposeSnipFunction', () => {
-        const Vue = getVue()
+        const Vue = getMockVue()
 
+        // @ts-ignore
         plugin.install(Vue, { exposeSnipFunction: true })
 
         expect((new Vue())[`$${defaultOptions.snipFunctionName}`]).not.eq(undefined)
       })
 
       it('Uses the given snipFunctionName', () => {
-        const Vue = getVue()
+        const Vue = getMockVue()
         const snipFunctionName = 'test'
 
+        // @ts-ignore
         plugin.install(Vue, { exposeSnipFunction: true, snipFunctionName: snipFunctionName })
 
         expect((new Vue())[`$${snipFunctionName}`]).not.eq(undefined)
@@ -153,35 +160,37 @@ describe('Vue Plugin', () => {
     })
   })
 
-  // describe('Compatibility', () => {
-  //   it('Handles Vue 2', () => {
-  //     let directiveResult = null
-  //
-  //     plugin.install(getVue('2.0.0', (directiveName, directiveOptions) => {
-  //       directiveResult = directiveOptions
-  //     }))
-  //
-  //     expect(directiveResult.mounted).equal(undefined)
-  //     expect(directiveResult.updated).equal(undefined)
-  //     expect(directiveResult.unmounted).equal(undefined)
-  //     expect(directiveResult.inserted).not.equal(undefined)
-  //     expect(directiveResult.update).not.equal(undefined)
-  //     expect(directiveResult.unbind).not.equal(undefined)
-  //   })
-  //
-  //   it('Handles Vue 3', () => {
-  //     let directiveResult = null
-  //
-  //     plugin.install(getVue('3.0.0', (directiveName, directiveOptions) => {
-  //       directiveResult = directiveOptions
-  //     }))
-  //
-  //     expect(directiveResult.mounted).not.equal(undefined)
-  //     expect(directiveResult.updated).not.equal(undefined)
-  //     expect(directiveResult.unmounted).not.equal(undefined)
-  //     expect(directiveResult.inserted).equal(undefined)
-  //     expect(directiveResult.update).equal(undefined)
-  //     expect(directiveResult.unbind).equal(undefined)
-  //   })
-  // })
+  describe('Compatibility', () => {
+    it('Handles Vue 2', () => {
+      let directiveResult = null
+
+      // @ts-ignore
+      plugin.install(getMockVue('2.0.0', (directiveName, directiveOptions) => {
+        directiveResult = directiveOptions
+      }))
+
+      expect(directiveResult.mounted).equal(undefined)
+      expect(directiveResult.updated).equal(undefined)
+      expect(directiveResult.unmounted).equal(undefined)
+      expect(directiveResult.inserted).not.equal(undefined)
+      expect(directiveResult.update).not.equal(undefined)
+      expect(directiveResult.unbind).not.equal(undefined)
+    })
+
+    it('Handles Vue 3', () => {
+      let directiveResult = null
+
+      // @ts-ignore
+      plugin.install(getMockVue('3.0.0', (directiveName, directiveOptions) => {
+        directiveResult = directiveOptions
+      }))
+
+      expect(directiveResult.mounted).not.equal(undefined)
+      expect(directiveResult.updated).not.equal(undefined)
+      expect(directiveResult.unmounted).not.equal(undefined)
+      expect(directiveResult.inserted).equal(undefined)
+      expect(directiveResult.update).equal(undefined)
+      expect(directiveResult.unbind).equal(undefined)
+    })
+  })
 })
