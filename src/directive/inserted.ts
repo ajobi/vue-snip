d90cccb4606e8e0b57e61985e186368c93284e8b
Andrej Bilec <andrej.bilec@gmail.com>
2022-03-05 21:07:59 +0100

    :package: New: Options moved to the directive value

--- src/directive/inserted.ts
+++ src/directive/inserted.ts
@@ -1,5 +1,5 @@
 import { snip } from 'js-snip'
 
-export const inserted = (el: HTMLElement, { value, arg }) => {
-  snip(el, { lines: value, mode: arg })
+export const inserted = (el: HTMLElement, { value }) => {
+  snip(el, value)
 }
