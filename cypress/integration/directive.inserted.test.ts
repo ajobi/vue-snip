a735ad67b00f7de779b08fdcd3c03c8c43fac972
Andrej Bilec <andrej.bilec@gmail.com>
2022-03-10 20:54:23 +0100

    :ok_hand: Improve: Tests already in the library removed

--- cypress/integration/directive.inserted.test.ts
+++ cypress/integration/directive.inserted.test.ts
@@ -1,63 +1,63 @@
-import { getInserted } from '../../instrumented/directive'
-import { defaultOptions } from '../../instrumented/defaultOptions'
-import { destroyObserver } from '../../instrumented/utils'
-
-describe('Directive Inserted', () => {
-  beforeEach(() => {
-    cy.visit('./cypress/tests/paragraph-single.html')
-  })
-
-  it('Adds the element to the map', () => {
-    cy.get('[data-cy=paragraph]').then(($paragraph) => {
-      const paragraph = $paragraph.get()[0]
-      const elementMap = new WeakMap()
-      const state = { elementMap, options: defaultOptions }
-
-      const snipText = cy.stub()
-      const inserted = getInserted(state, snipText)
-
-      inserted(paragraph, { value: 3, arg: 'css' })
-
-      expect(elementMap.has(paragraph)).to.equal(true)
-    })
-  })
-
-  describe('Snips the element', () => {
-    it('With CSS method', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        const elementMap = new WeakMap()
-        const state = { elementMap, options: defaultOptions }
-
-        const snipText = cy.stub()
-        const inserted = getInserted(state, snipText)
-
-        inserted(paragraph, { value: 3, arg: 'css' })
-        expect(snipText).to.have.callCount(1)
-        expect(snipText).to.be.calledWith(paragraph)
-      })
-    })
-
-    it('With JS method', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        const elementMap = new WeakMap()
-        const state = { elementMap, options: defaultOptions }
-
-        const snipText = cy.stub()
-        const inserted = getInserted(state, snipText)
-        elementMap.set(paragraph, { fullText: paragraph.textContent })
-
-        inserted(paragraph, { value: 3, arg: 'js' })
-
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(20).then(() => {
-          expect(snipText).to.have.callCount(1)
-          expect(snipText).to.be.calledWith(paragraph)
-
-          destroyObserver(state, paragraph)
-        })
-      })
-    })
-  })
-})
+// import { getInserted } from '../../instrumented/directive'
+// import { defaultOptions } from '../../instrumented/defaultOptions'
+// import { destroyObserver } from '../../instrumented/utils'
+//
+// describe('Directive Inserted', () => {
+//   beforeEach(() => {
+//     cy.visit('./cypress/tests/paragraph-single.html')
+//   })
+//
+//   it('Adds the element to the map', () => {
+//     cy.get('[data-cy=paragraph]').then(($paragraph) => {
+//       const paragraph = $paragraph.get()[0]
+//       const elementMap = new WeakMap()
+//       const state = { elementMap, options: defaultOptions }
+//
+//       const snipText = cy.stub()
+//       const inserted = getInserted(state, snipText)
+//
+//       inserted(paragraph, { value: 3, arg: 'css' })
+//
+//       expect(elementMap.has(paragraph)).to.equal(true)
+//     })
+//   })
+//
+//   describe('Snips the element', () => {
+//     it('With CSS method', () => {
+//       cy.get('[data-cy=paragraph]').then(($paragraph) => {
+//         const paragraph = $paragraph.get()[0]
+//         const elementMap = new WeakMap()
+//         const state = { elementMap, options: defaultOptions }
+//
+//         const snipText = cy.stub()
+//         const inserted = getInserted(state, snipText)
+//
+//         inserted(paragraph, { value: 3, arg: 'css' })
+//         expect(snipText).to.have.callCount(1)
+//         expect(snipText).to.be.calledWith(paragraph)
+//       })
+//     })
+//
+//     it('With JS method', () => {
+//       cy.get('[data-cy=paragraph]').then(($paragraph) => {
+//         const paragraph = $paragraph.get()[0]
+//         const elementMap = new WeakMap()
+//         const state = { elementMap, options: defaultOptions }
+//
+//         const snipText = cy.stub()
+//         const inserted = getInserted(state, snipText)
+//         elementMap.set(paragraph, { fullText: paragraph.textContent })
+//
+//         inserted(paragraph, { value: 3, arg: 'js' })
+//
+//         // eslint-disable-next-line cypress/no-unnecessary-waiting
+//         cy.wait(20).then(() => {
+//           expect(snipText).to.have.callCount(1)
+//           expect(snipText).to.be.calledWith(paragraph)
+//
+//           destroyObserver(state, paragraph)
+//         })
+//       })
+//     })
+//   })
+// })
--- cypress/integration/directive.unbind.test.ts
+++ cypress/integration/directive.unbind.test.ts
@@ -1,24 +1,24 @@
-import { getUnbind } from '../../instrumented/directive'
-import { defaultOptions } from '../../instrumented/defaultOptions'
-
-describe('Directive Unbind', () => {
-  beforeEach(() => {
-    cy.visit('./cypress/tests/paragraph-single.html')
-  })
-
-  it('Removes the elements from the map', () => {
-    cy.get('[data-cy=paragraph]').then(($paragraph) => {
-      const paragraph = $paragraph.get()[0]
-
-      const elementMap = new WeakMap()
-      const state = { elementMap, options: defaultOptions }
-
-      const unbind = getUnbind(state)
-
-      elementMap.set(paragraph, {})
-      unbind(paragraph)
-
-      expect(elementMap.has(paragraph)).to.equal(false)
-    })
-  })
-})
+// import { getUnbind } from '../../instrumented/directive'
+// import { defaultOptions } from '../../instrumented/defaultOptions'
+//
+// describe('Directive Unbind', () => {
+//   beforeEach(() => {
+//     cy.visit('./cypress/tests/paragraph-single.html')
+//   })
+//
+//   it('Removes the elements from the map', () => {
+//     cy.get('[data-cy=paragraph]').then(($paragraph) => {
+//       const paragraph = $paragraph.get()[0]
+//
+//       const elementMap = new WeakMap()
+//       const state = { elementMap, options: defaultOptions }
+//
+//       const unbind = getUnbind(state)
+//
+//       elementMap.set(paragraph, {})
+//       unbind(paragraph)
+//
+//       expect(elementMap.has(paragraph)).to.equal(false)
+//     })
+//   })
+// })
