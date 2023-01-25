c157d5cf4dcf4fbf40756fd061ebd28d02436d76
revelstoke-tim <tim@revelstoke.io>
2023-01-25 15:10:49 -0500

    Fix for data update.

--- src/directive/updated.ts
+++ src/directive/updated.ts
@@ -1,7 +1,12 @@
-import { snip } from 'js-snip'
+import { snip, unsnip } from 'js-snip'
 import { VueSnipDirectiveHook } from '../types'
 
-export const updated: VueSnipDirectiveHook = (el, { value }) => {
+export const updated: VueSnipDirectiveHook = (el, { value }, vnode) => {
+  unsnip(el)
+  const text = vnode?.children?.[0]?.el?.textContent
+  if (text !== undefined) {
+    el.textContent = text
+  }
   const { lines, mode, midWord, ellipsis, onSnipped } = value
   snip(el, { lines, mode, midWord, ellipsis }, onSnipped)
 }
