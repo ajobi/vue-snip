daeb90092b8086052b346ea28b7d93ff5f5a22a5
Andrej Bilec <andrej.bilec@gmail.com>
2023-02-05 13:06:34 +0100

    :wrench: Fix: Use textContent option to update snipped text value

--- src/directive/updated.ts
+++ src/directive/updated.ts
@@ -1,12 +1,8 @@
-import { snip, unsnip } from 'js-snip'
+import { snip } from 'js-snip'
 import { VueSnipDirectiveHook } from '../types'
 
-export const updated: VueSnipDirectiveHook = (el, { value }, vnode) => {
-  unsnip(el)
-  const text = vnode?.children?.[0]?.el?.textContent
-  if (text !== undefined) {
-    el.textContent = text
-  }
+export const updated: VueSnipDirectiveHook = (el, { value }, vNode) => {
+  const { text: textContent } = vNode?.children?.[0]
   const { lines, mode, midWord, ellipsis, onSnipped } = value
-  snip(el, { lines, mode, midWord, ellipsis }, onSnipped)
+  snip(el, { lines, mode, midWord, ellipsis, textContent }, onSnipped)
 }
