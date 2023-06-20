eda012e921f6e386e71b6f508ea55b5713f6ac5c
Andrej <andrej.bilec@gmail.com>
2023-06-20 15:56:55 +0200

    :book: Doc: documents todos

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { mounted, updated, unmounted } from './directive'
 import { Plugin } from 'vue'
 
+/*
+ * TODOs:
+ * - remove the issues shield
+ * - migrate from travis to GitHub Actions
+ * */
 export default ((): Plugin => ({
   install(Vue) {
     const isVue3 = parseFloat(Vue.version[0]) > 2
