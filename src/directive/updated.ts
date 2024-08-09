5b37b121025408381f694c9e9ae5f35eba9ea560
Marco Quintella <2om.marco.quintella@gmail.com>
2024-08-09 14:50:42 -0300

    fix : Text doesn't update when data is updated

--- src/directive/updated.ts
+++ src/directive/updated.ts
@@ -2,7 +2,7 @@ import { snip } from 'js-snip'
 import { VueSnipDirectiveHook } from '../types'
 
 export const updated: VueSnipDirectiveHook = (el, { value }, vNode) => {
-  const textContent = vNode?.children?.[0]?.text ?? ''
+  const textContent = vNode?.children?.[0]?.el?.contentText ?? ''
   const { lines, mode, midWord, ellipsis, onSnipped } = value
   snip(el, { lines, mode, midWord, ellipsis, textContent }, onSnipped)
 }
