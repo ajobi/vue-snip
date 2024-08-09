81fd64c877cc222a815b725e7624668c2f685a55
Marco Quintella <2om.marco.quintella@gmail.com>
2024-08-09 14:55:30 -0300

    Update updated.ts

--- src/directive/updated.ts
+++ src/directive/updated.ts
@@ -2,7 +2,7 @@ import { snip } from 'js-snip'
 import { VueSnipDirectiveHook } from '../types'
 
 export const updated: VueSnipDirectiveHook = (el, { value }, vNode) => {
-  const textContent = vNode?.children?.[0]?.el?.contentText ?? ''
+  const textContent = vNode?.children?.[0]?.el?.textContent ?? ''
   const { lines, mode, midWord, ellipsis, onSnipped } = value
   snip(el, { lines, mode, midWord, ellipsis, textContent }, onSnipped)
 }
